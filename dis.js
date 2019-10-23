c65787a02e2dffd40728c5eec3bc7d14cb5f26b9
DARUBI <00121117@uca.edu.sv>
2019-10-23 16:45:33 -0600

    Segundo commit

--- dis.js
+++ dis.js
@@ -5,10 +5,19 @@ let btn = document.querySelector("#a_sumar")
 let num1=document.querySelector("#num1")
 let num2=document.querySelector("#num2")
 
+
+
+
 btn.addEventListener("click",()=>
 {
 
+
     result.textContent=(parseInt(num1.value)+parseInt(num2.value))
+
+    document.getElementById('imagenresult').src="./images/JPEG/Trebol/10C.jpg"
+
+    
+
 }
 
 )
