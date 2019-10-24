b87d0db06bf529acf738cb9e63f02bdcd78155f9
DARUBI <00121117@uca.edu.sv>
2019-10-24 11:43:28 -0600

    ya termine lo de agrgar foto

--- dis.js
+++ dis.js
@@ -1,23 +1,68 @@
 let result = document.querySelector("#result")
-
 let btn = document.querySelector("#a_sumar")
-
 let num1=document.querySelector("#num1")
 let num2=document.querySelector("#num2")
-
-
-
-
-btn.addEventListener("click",()=>
-{
-
-
-    result.textContent=(parseInt(num1.value)+parseInt(num2.value))
-
-    document.getElementById('imagenresult').src="./images/JPEG/Trebol/10C.jpg"
-
+let padre=document.querySelector("#padre")
+var repImage;
+function myFunction() { 
+    var retvalue = Math.floor((Math.random() * 10) + 1);
+    //de treboles
+        switch (retvalue){
+            case 1:
+                repImage ="<img src='images/JPEG/Trebol/AC.jpg' width='100' height='150'>";
+                return repImage;
+                break;
+            case 2:
+                repImage ="<img src='images/JPEG/Trebol/2C.jpg' width='100' height='150'>";
+                return repImage;
+                break;   
+            case 3:
+                repImage ="<img src='images/JPEG/Trebol/3C.jpg' width='100' height='150'>";
+                return repImage;
+                break;    
+            case 4:
+                repImage ="<img src='images/JPEG/Trebol/4C.jpg' width='100' height='150'>";
+                return repImage;
+                break;    
+            case 5:
+                repImage ="<img src='images/JPEG/Trebol/5C.jpg' width='100' height='150'>";
+                return repImage;
+                break;    
+            case 6:
+                repImage ="<img src='images/JPEG/Trebol/6C.jpg' width='100' height='150'>";
+                return repImage;
+                break;    
+            case 7:
+                repImage ="<img src='images/JPEG/Trebol/7C.jpg' width='100' height='150'>";
+                return repImage;
+                break;    
+            case 8:
+                repImage ="<img src='images/JPEG/Trebol/8C.jpg' width='100' height='150'>";
+                return repImage;
+                break;    
+            case 9:
+                repImage ="<img src='images/JPEG/Trebol/9C.jpg' width='100' height='150'>";
+                return repImage;
+                break;    
+            case 10:
+                repImage ="<img src='images/JPEG/Trebol/10C.jpg' width='100' height='150'>";
+                return repImage;
+                break;                                                                                  
+            default:
+                return NO
+      
+    }
+  
     
-
 }
+btn.addEventListener("click",()=>{
+   
+    document.getElementById('imagenresult').src="./images/JPEG/Trebol/10C.jpg"
+    var table = document.getElementById("tablero");
+    var cell1 = padre.insertCell(-1);
+    cell1.innerHTML = myFunction()
+   // cell1.innerHTML=retvalue
+})
 
-)
+
+
